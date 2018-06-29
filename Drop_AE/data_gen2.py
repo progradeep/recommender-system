0dd2915290deee3646fa54145bd0d4b7a4640f88
JaeyunKang <kjy13411@gmail.com>
2018-06-29 10:37:24 +0000

    changes in dropae

--- Drop_AE/data_gen2.py
+++ Drop_AE/data_gen2.py
@@ -1,27 +1,27 @@
 from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from time import time
 
 input_path = "../../data/KISA_TBC_VIEWS_UNIQ.csv"
-output_path = "../../data/short1.csv"
+output_path = "../../data/short_end.csv"
 
 with open(input_path, "r") as file:
-    input = file.readlines()
+    data = file.readlines()
+    data = np.asarray(data)
+data = np.genfromtxt(input_path, delimiter=",",dtype=int)
+data = data[1:,:2]
 
-dict = {}
+f = open(output_path, 'w')
+start = time()
 
-for i, line in tqdm(enumerate(input)):
-    input[i] = line.split(",")[:2]
-    if not input[i][0] in dict.keys():
-        dict[input[i][0]] = [input[i][1]]
-    else:
-        dict[input[i][0]].append(input[i][1])
-    if input[i][0] == '60000': break
+for user in range(200000,len(data)):
+    ind = np.where(data[:,0]==user)
+    tmp = data[ind[0],1].tolist()
+    f.write(str(tmp)[1:-1]+'\n')
+    if user % 100 == 0:
+	    end = time()
+	    print("user", user, "done!",end-start)
 
-    print(input[i])
-
-with open(output_path, "w") as file:
-    for v in dict.values():
-        line = str(v)[1:-1].replace(" ","")
-
-        line += "\n"
-
-        file.write(line)
+print("res done")
+f.close()
