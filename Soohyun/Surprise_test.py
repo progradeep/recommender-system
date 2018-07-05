beff4c74066479b73f785085105be09e35d8c009
tngusqkr1 <tngusqkr1@gmail.com>
2018-07-05 23:42:14 +0900

    .

--- Soohyun/Surprise_test.py
+++ Soohyun/Surprise_test.py
@@ -1,11 +1,49 @@
 import os
 import pandas as pd
+from collections import defaultdict
 from surprise import Dataset
 from surprise import Reader
 from surprise import SVD
 from surprise import accuracy
 
 from surprise.model_selection import train_test_split
+
+a = [['a','b','c'],[1,2,3],[1,3,4],[2,2,4]]
+d = pd.DataFrame(a, columns=['a','b','c'])
+print(d)
+
+r = d.loc[d['a']==1]
+print(r)
+print(2 in r['b'])
+
+
+
+def get_top_n(predictions, testdf, n=50):
+    '''Return the top-N recommendation for each user from a set of predictions.
+
+    Args:
+        predictions(list of Prediction objects): The list of predictions, as
+            returned by the test method of an algorithm.
+        n(int): The number of recommendation to output for each user. Default
+            is 10.
+
+    Returns:
+    A dict where keys are user (raw) ids and values are lists of tuples:
+        [(raw item id, rating estimation), ...] of size n.
+    '''
+
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        if iid not in test_df.loc[test_df['USER_ID']==uid]['MOVIE_ID']:
+            top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
 
 # path to dataset file
 file_path = '../../data/KISA_TBC_VIEWS_UNIQ.csv'
@@ -21,13 +59,20 @@ df['RATING'] = [1] * len(df['USER_ID'])
 reader = Reader(line_format='user item rating', sep=',')
 
 data = Dataset.load_from_df(df, reader=reader)
+trainset = data.build_full_trainset()
 
-trainset, testset = train_test_split(data, test_size=.25)
+test_df = df[df['USER_ID'].gt(393186)]
+data = Dataset.load_from_df(test_df, reader=reader)
+testset = data.build_full_trainset()
 
 # We'll use the famous SVD algorithm.
-algo = SVD()
-algo.fit(trainset, verbose = True, n_epochs = 5)
-predictions = algo.test(testset)
+algo = SVD(verbose = True, n_epochs = 5)
+algo.fit(trainset)
+predictions = algo.test(testset, verbose=True)
 
 # Then compute RMSE
 accuracy.rmse(predictions)
+top_n = get_top_n(predictions, test_df, n=50)
+
+output = pd.DataFrame(top_n)
+output.to_excel("./output.xlsx")
