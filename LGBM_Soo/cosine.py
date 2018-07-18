eed76b35c6d46ee23a75cf3681654fb816bac681
tngusqkr1 <tngusqkr1@gmail.com>
2018-07-18 23:20:35 +0900

    cal score

--- LGBM_Soo/cosine.py
+++ LGBM_Soo/cosine.py
@@ -4,7 +4,7 @@ import scipy.sparse as sp
 from scipy.sparse import csc_matrix
 import sklearn.preprocessing as pp
 
-
+"""
 dataroot = "../../data/"
 def cosine_similarities(sp_mat):
     col_normed_mat = pp.normalize(sp_mat, axis=0)
@@ -27,3 +27,31 @@ print("mat saved!")
 
 cos = cosine_similarities(data)
 np.save(dataroot+"cosine_sim.npy",cos)
+"""
+
+# score(i, x) = (itemsSimilarity(x, record(i, 1)) + … + itemsSimilarity(x, record(i, M(i)))) /
+# (itemsSimilarity(x, item(1)) + … + itemsSimilarity(x, item(K)))
+# (단, 식 표현 편의를 위해 itemsSimilarity(x, x) = 0 으로 가정한 식입니다.)
+cos = np.load("../../data/cosine_sim.npy")
+test = pd.read_csv("../../data/KISA_TBC_VIEWS_UNIQ_TEST.csv")
+
+def calculate_sim(row):
+    global cos
+    user_id = row['USER_ID'].values[0]
+    items = row['MOVIE_ID'].values
+
+    out = []
+
+    for i in range(cos.shape[0]):
+        score = 0.
+        for m in items:
+            score += cos[i,int(m)]
+        score /= sum(cos[i,:])
+
+        out.append(score)
+
+    return user_id+sorted(range(len(out)), key=lambda i: out[i], reverse=True)[:50]
+
+
+grouped = test.groupby(by='USER_ID').apply(calculate_sim).tolist()
+
